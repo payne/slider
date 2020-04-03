7a508c6304a13d5b50abc698a468af02cf8c8b19
Matt Payne <Payne@MattPayne.org>
2020-04-03 17:21:35 -0500

    ng generate component slider1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Slider1Component } from './slider1/slider1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Slider1Component
   ],
   imports: [
     BrowserModule,

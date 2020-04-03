5bd64280495579f5fdc2daca7c4fc7380f6e27a2
Matt Payne <Payne@MattPayne.org>
2020-04-03 18:24:53 -0500

    Added a vertical slider

--- src/app/slider1/slider1.component.ts
+++ src/app/slider1/slider1.component.ts
@@ -8,17 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Slider1Component implements OnInit {
   value: number;
+  vvalue: number;
   enabled: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
     this.value=10;
+    this.vvalue=90;
     this.enabled=true;
   }
 
   change() {
     console.log(`change function value=${this.value}`);
+  }
+  vchange() {
+    console.log(`vchange function value=${this.vvalue}`);
   }
 
 
